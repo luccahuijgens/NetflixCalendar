688f1e71cebbe6949319ba36bd5e90cdebf9508c
graddus <luccagraddushuijgens@gmail.com>
2017-06-19 16:49:28 +0200

    Delete SerieServiceProvider.java

--- src/java/nl/hu/v1ipass/thirdapp/model/SerieServiceProvider.java
+++
@@ -1,9 +0,0 @@
-package nl.hu.v1ipass.thirdapp.model;
-
-public class SerieServiceProvider {
-	private static SeriesService customerservice = new SeriesService();
-
-	public static SeriesService getSeriesService() {
-		return customerservice;
-	}
-}
