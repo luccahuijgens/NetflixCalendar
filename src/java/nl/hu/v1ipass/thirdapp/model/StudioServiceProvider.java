46f9e636b54ee74b9f2cb643d606911747074fdf
graddus <luccagraddushuijgens@gmail.com>
2017-06-19 16:49:39 +0200

    Delete StudioServiceProvider.java

--- src/java/nl/hu/v1ipass/thirdapp/model/StudioServiceProvider.java
+++
@@ -1,9 +0,0 @@
-package nl.hu.v1ipass.thirdapp.model;
-
-public class StudioServiceProvider {
-	private static StudioService customerservice = new StudioService();
-
-	public static StudioService getStudioService() {
-		return customerservice;
-	}
-}
