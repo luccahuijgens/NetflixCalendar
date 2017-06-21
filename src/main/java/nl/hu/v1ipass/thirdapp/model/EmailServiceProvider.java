b62bc8f1958876773dbfde3ec50ec9bc57a536e6
graddus <luccagraddushuijgens@gmail.com>
2017-06-21 14:14:53 +0200

    Delete EmailServiceProvider.java

--- src/main/java/nl/hu/v1ipass/thirdapp/model/EmailServiceProvider.java
+++
@@ -1,9 +0,0 @@
-package nl.hu.v1ipass.thirdapp.model;
-
-public class EmailServiceProvider {
-	private static EmailService customerservice = new EmailService();
-
-	public static EmailService getEmailService() {
-		return customerservice;
-	}
-}
